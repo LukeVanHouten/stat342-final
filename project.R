7ab0e0ddc06628a3ebdec2da10ceb9d5ac48bce3
LukeVanHouten <lukevh@uw.edu>
2024-05-22 13:03:24 -0700

    Viz

--- project.R
+++ project.R
@@ -1,9 +1,27 @@
 library(vcd)
+library(tidyverse)
+library(reshape2)
 
-data <- as.matrix(read.csv("data.csv", row.names=1))
+data <- read.csv("data.csv")
 
-mosaic(data, gp = gpar(fill = c("lightblue", "pink", "red", "blue")),
-       labeling = labeling_values,
-       labeling_args = list(
-           set_varnames = c(Covid = "COVID", No_Covid = "No COVID"),
-           set_labels = list(Covid = rownames(data))))
+data_mosaic <- as.matrix(data[, -1])
+rownames(data_mosaic) <- data[, 1]
+
+proportion_df <- data.frame(
+    Test = data$Test,
+    COVID = c((data$COVID[[1]] / (data$COVID[[1]] + data$COVID[[2]])),
+              (data$COVID[[2]] / (data$COVID[[1]] + data$COVID[[2]]))),
+    No_COVID = c((data$No_COVID[[1]] / (data$No_COVID[[1]] + 
+                                        data$No_COVID[[2]])),
+                 (data$No_COVID[[2]] / (data$No_COVID[[1]] + 
+                                        data$No_COVID[[2]])))
+)
+
+mosaic(data_mosaic, gp = gpar(fill = c("lightblue", "pink", "red", "blue")),
+       labeling = labeling_values)
+
+proportion_df <- melt(proportion_df, id.vars = "Test", 
+                      variable.name = "Result", value.name = "Proportion")
+
+ggplot(data = proportion_df, aes(x = Result, y = Proportion, group = Test)) +
+    geom_boxplot(aes(color = Test))
