7d271f909290495a64a5b0a3743764acd95a4fa1
LukeVanHouten <lukevh@uw.edu>
2024-05-24 12:32:39 -0700

    idek atp

--- project.R
+++ project.R
@@ -17,11 +17,30 @@ proportion_df <- data.frame(
                                         data$No_COVID[[2]])))
 )
 
+t_data <- as.data.frame(t(data[, 2:3]))
+colnames(t_data) <- c("Vaccine", "Placebo")
+
+proportion_df2 <- data.frame(
+    Test = rownames(t_data),
+    Vaccine = c((t_data$Vaccine[[1]] / (t_data$Vaccine[[1]] +
+                                        t_data$Vaccine[[2]])),
+                (t_data$Vaccine[[2]] / (t_data$Vaccine[[1]] + 
+                                        t_data$Vaccine[[2]]))),
+    Placebo = c((t_data$Placebo[[1]] / (t_data$Placebo[[1]] + 
+                                        t_data$Placebo[[2]])),
+                (t_data$Placebo[[2]] / (t_data$Placebo[[1]] + 
+                                        t_data$Placebo[[2]]))))
+
+colnames(proportion_df2) <- c("Diagnosis", "Vaccine", "Placebo")
+
 mosaic(data_mosaic, gp = gpar(fill = c("lightblue", "pink", "red", "blue")),
        labeling = labeling_values)
 
-proportion_df <- melt(proportion_df, id.vars = "Test", 
+proportion_df_melted <- melt(proportion_df, id.vars = "Test", 
                       variable.name = "Result", value.name = "Proportion")
 
-ggplot(data = proportion_df, aes(x = Result, y = Proportion, group = Test)) +
-    geom_boxplot(aes(color = Test))
+proportion_df_2_melted <- melt(proportion_df2, id.vars = "Diagnosis", 
+                               variable.name = "Result", value.name = "Proportion")
+
+ggplot(data = proportion_df_2_melted, aes(x = Result, y = Proportion, group = Diagnosis)) +
+    geom_boxplot(aes(color = Diagnosis))
